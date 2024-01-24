a717035e5edbaae3fadb02168cabddb8a69d397a
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-01-23 23:48:11 -0300

    feat: criado exemplo funcional para o princípio OCP

---
+++ OCP/Program.cs
@@ -0,0 +1,13 @@
+﻿using OCP;
+
+decimal valorProduto = 200;
+
+// calculadora de imposto: produto padrão
+var calculadoraPadrao = CalculadoraImpostoFactory.CriarCalculadoraImposto(TipoProduto.Padrao);
+decimal impostoPadrao = calculadoraPadrao.CalcularImpostoParaProduto(valorProduto);
+Console.WriteLine($"Imposto para produto padrão: {impostoPadrao:C}");
+
+// calculadora de imposto:  produto importado
+var calculadoraImportado = CalculadoraImpostoFactory.CriarCalculadoraImposto(TipoProduto.Importado);
+decimal impostoImportado = calculadoraImportado.CalcularImpostoParaProduto(valorProduto);
+Console.WriteLine($"Imposto para produto importado: {impostoImportado:C}");
