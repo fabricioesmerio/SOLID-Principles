27368e7a845b23e38ea4d110f9992e34bfef8d2e
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-01-24 00:15:50 -0300

    feat: adicionado implementação do princípio ISP

---
+++ ISP/Program.cs
@@ -0,0 +1,15 @@
+﻿using ISP;
+
+Escola minhaEscola = new Escola();
+
+IAluno aluno1 = new Aluno { Nome = "Joãozinho", Idade = 10 };
+IProfessor professor1 = new Professor { Nome = "Mariazinha", Idade = 35 };
+
+minhaEscola.AdicionarAluno(aluno1);
+minhaEscola.AdicionarProfessor(professor1);
+
+aluno1.Matricular();
+aluno1.Estudar();
+
+professor1.DarAula();
+professor1.AvaliarAluno();
