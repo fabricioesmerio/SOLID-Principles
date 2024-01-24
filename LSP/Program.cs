7955c5b9024f95c8fedf682e480b2ff3d1242572
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-01-24 00:03:19 -0300

    feat: criado código para testar implementação do LSP

---
+++ LSP/Program.cs
@@ -0,0 +1,10 @@
+﻿using LSP;
+
+
+Desenvolvedor desenvolvedor = new Desenvolvedor { Nome = "Fabricio" };
+SistemaRecursosHumanos.RealizarTarefaEImprimir(desenvolvedor);
+SistemaRecursosHumanos.RealizarHabilidadeEspecialEImprimir(desenvolvedor);
+
+GerenteProjeto gerenteProjeto = new GerenteProjeto { Nome = "Alguém" };
+SistemaRecursosHumanos.RealizarTarefaEImprimir(gerenteProjeto);
+SistemaRecursosHumanos.RealizarHabilidadeEspecialEImprimir(gerenteProjeto);
