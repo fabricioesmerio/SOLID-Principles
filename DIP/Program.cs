429f1d4bcf6f2f07c412240ad0b7ac5b92e47951
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-01-24 00:24:43 -0300

    feat: Implementado código para validar o DIP

---
+++ DIP/Program.cs
@@ -0,0 +1,9 @@
+﻿using DIP;
+
+IPagamento metodoPagamentoComCartao = new PagamentoCartaoCredito();
+ServicoPagamento servicoPgtoCartao = new ServicoPagamento(metodoPagamentoComCartao);
+servicoPgtoCartao.RealizarPagamento(135.00m);
+
+IPagamento metodoPagamentoComPayPal = new PagamentoPayPal();
+ServicoPagamento servicoPgtoPayPal = new ServicoPagamento(metodoPagamentoComPayPal);
+servicoPgtoPayPal.RealizarPagamento(95.34m);
